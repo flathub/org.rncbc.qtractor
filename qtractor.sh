8286b6c1ee2f38195fb11f80c337db097d5339fa
Hubert Figuière <hub@figuiere.net>
2022-05-25 18:19:23 -0400

    Issue #7 - Fix VST and VST3 path

--- qtractor.sh
+++ qtractor.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 export LV2_PATH=$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2
+export VST3_PATH=/app/extensions/Plugins/vst3:$HOME/.vst3
+export VST_PATH=/app/extensions/Plugins/vst:$HOME/.vst
 
 exec qtractor.bin $*
