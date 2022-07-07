a449984c55410ab2c01999e61ad5a77fffaaa43a
Rui Nuno Capela <rncbc@rncbc.org>
2022-07-07 18:00:15 +0100

    Update qtractor.sh

--- qtractor.sh
+++ qtractor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LV2_PATH=$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2
+export CLAP_PATH=/app/extensions/Plugins/clap:$HOME/.clap
 export VST3_PATH=/app/extensions/Plugins/vst3:$HOME/.vst3
 export VST_PATH=/app/extensions/Plugins/vst:$HOME/.vst
 
