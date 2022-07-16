59d1e31f7dee82a3822919ebf4267687f59a06ba
Rui Nuno Capela <rncbc@rncbc.org>
2022-07-16 17:47:19 +0100

    Update qtractor.sh

--- qtractor.sh
+++ qtractor.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-export LV2_PATH=$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2
-export CLAP_PATH=/app/extensions/Plugins/clap:$HOME/.clap
-export VST3_PATH=/app/extensions/Plugins/vst3:$HOME/.vst3
-export VST_PATH=/app/extensions/Plugins/vst:$HOME/.vst
+export LADSPA_PATH="$HOME/.ladspa:/app/extensions/Plugins/ladspa:/app/lib/ladspa"
+export DSSI_PATH="$HOME/.dssi:/app/extensions/Plugins/dssi"
+export LV2_PATH="$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2"
+export VST_PATH="$HOME/.vst:/app/extensions/Plugins/lxvst"
+export VST3_PATH="$HOME/.vst3:/app/extensions/Plugins/vst3"
+export CLAP_PATH="$HOME/.clap:/app/extensions/Plugins/clap"
 
 exec qtractor.bin $*
